46e75096e0311f79feb388fecf49b8789275f548
Jay Toy <jay_toyjianying@hotmail.com>
2021-11-24 12:31:02 +0100

    first commit

---
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
@@ -0,0 +1,40 @@
+package com.example.moviecatalogservice.resources;
+
+import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        // Get all rated movie IDs
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+
+        // For each movie ID, call movie info service and get details
+        return ratingsList.stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList()); // Put them all together
+    }
+}
